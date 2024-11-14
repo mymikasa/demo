b3bc1c92921068a3b1b244cd877ebb51299239c2
13426 <2775567503@qq.com>
2024-11-14 21:59:05 +0800

    qwer

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld {
+    public static void main(String[]arges)
+    {           System.out.println("helloworld");
+
+        int a=10;
+        int b=20;
+
+        System.out.println(a+b);
+    }
+
+}
